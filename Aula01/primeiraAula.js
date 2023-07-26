a92f65b75dfaf09867773126a55d843b7c5da8e1
JOÃO HENRIQUE MARTINS GENESIO <23143159@edu119.local>
2023-07-26 13:35:24 -0300

    Entrega das atividades

---
+++ Aula01/primeiraAula.js
@@ -0,0 +1,49 @@
+console.log('17')
+
+console.log('2 semanas programando') //Programei em casa
+
+console.log('Atividade 3')/*Essa é atividade 3,
+que solicita um comentário em várias linhas
+*/
+
+let x1 = 1 //Number
+let x2 = 'oi' //String
+let x3 = true //Boolean
+let x4 = null //Null
+let x5 = undefined //Undefined
+const eu = {
+    name: 'João',
+    idade: 17
+} //Objects
+
+console.log(17+3.5) //Aumentando minha idade em 3.5
+
+let now = new Date
+console.log(now.getFullYear()-1969 + ' anos se passaram quando o homem pisou na lua.') //Pisou na lua
+
+var a = 65/240
+console.log(a) //Resultado do calculo 65 dividido por 240
+
+var b = 0.2708*100
+console.log(b) //Resultado do calculo 0.2708 multiplicado por 100
+
+console.log('Hello' + 'World') //Vai aparecer 'HelloWorld'
+
+console.log('Hello' + ' ' + 'World') //Vai aparecer 'Hello World'
+
+console.log('Teaching the world how to code'.length) //Resultado 30
+
+console.log('Kaian Novais'.toUpperCase()) //Tudo em maiúsculo
+
+console.log('           Professor Kaian Novais'.trim()) //Tirou os espaços
+
+function getRandow() {
+    return Math.random() * 100;
+}
+console.log(getRandow()) // Número aleatório
+
+let intX = Math.ceil(43.8)
+console.log(intX) // Resultado 44
+
+let intY = Number.isInteger(2017)
+console.log(intY) // Resultado true
